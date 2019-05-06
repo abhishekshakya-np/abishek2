60fa59fcf50363f41bfd7e0a07eb1d62032eeb15
xabickx <abishek12344321@gmail.com>
2019-05-06 17:07:14 +0545

    if stament

---
+++ src/abishek2/newapples.java
@@ -0,0 +1,25 @@
+package abishek2;
+
+
+class newapples {
+	public static void main (String args []) {
+		int age;
+		age = 3;
+		
+		switch (age){ 
+		case 1 :			
+			System.out.println("you are child");
+		break;
+		case 2 :
+			System.out.println("you are boy");
+			break;
+			
+		case 3 :
+			System.out.println("you are young");
+			break;
+			
+		
+		}
+	}
+
+}
